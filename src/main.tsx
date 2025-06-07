8f992b6f2cd30afe8bdfae20d48f8c9be01120bb
said rassai <saidrassai@gmail.com>
2025-06-07 02:31:34 +0100

    WIP: Advanced optimizations - minimal app test, performance improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import './utils/sw-registration' // Register service worker
-import { initializeAnalytics } from './utils/analytics' // Import comprehensive analytics
+import AppMinimal from './App_minimal.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppMinimal />
   </StrictMode>,
 )
-
-// Initialize comprehensive performance monitoring
-initializeAnalytics();
